05993010fb9bdd93eb54d82d1c4fda477576d645
DC-21 <cholahkuboko@gmail.com>
2023-10-23 19:35:26 +0200

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Hello{
+        static void Main(String[] args){
+        Console.WriteLine("Hello World");
+        Console.ReadKey();
+    }
+    }
+}

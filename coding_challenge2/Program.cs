96c02505b7ab33e633b65e61819612da4b8d75f8
DC-21 <cholahkuboko@gmail.com>
2023-10-25 11:39:12 +0200

    challenge

---
+++ coding_challenge2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+if(daysUntilExpiration<=10){
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+if(daysUntilExpiration<=5){
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.Renew now and save 10%!");
+    discountPercentage = 10;
+}
+if(daysUntilExpiration==1){
+    Console.WriteLine("Your subscription expires within a day! Renew now and save 20%!");
+    discountPercentage = 20;
+}
+if(discountPercentage>0){
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}

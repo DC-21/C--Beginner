6b5d72dac2c8c87da8b14eb5fd209e3ffd3a35ac
DC-21 <cholahkuboko@gmail.com>
2023-10-25 10:37:29 +0200

    challenge

---
+++ method_challenge/Program.cs
@@ -0,0 +1,7 @@
+﻿//Code challenge: Implement a method of the Math class that returns the larger of two numbers
+
+using System;
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue,secondValue);
+Console.WriteLine(largerValue);

eb5c844b9467074ecafcc47e26ccdbb77b5783c5
DC-21 <cholahkuboko@gmail.com>
2023-10-26 08:48:08 +0200

    arrays

---
+++ aall_arrays/challenge/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] names = {"B123","C234","A345","C15","B177","G3003","C235","B179"};
+
+foreach(var name in names)
+{
+    if(name.StartsWith("B"))
+    {
+        Console.WriteLine(name);
+    }
+}

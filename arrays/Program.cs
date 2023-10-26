bdb66d07611a236d38cf44c20f2186c6e3f6afb6
DC-21 <cholahkuboko@gmail.com>
2023-10-26 07:53:08 +0200

    arrays

--- arrays/Program.cs
+++ arrays/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
-string[] fraudulentOrderIDs = new string[3];
 
-fraudulentOrderIDs[0]="001a";
-fraudulentOrderIDs[1]="002b";
-fraudulentOrderIDs[2]="003c";
+//declare an array of three elements
+//string[] fraudulentOrderIDs = new string[3];
 
+// fraudulentOrderIDs[0]="001a";
+// fraudulentOrderIDs[1]="002b";
+// fraudulentOrderIDs[2]="003c";
+
+//try this syntax
+string[] fraudulentOrderIDs = {"001a","002b","003c"};
+
+//print the value of elements in console
 Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
 Console.WriteLine($"First: {fraudulentOrderIDs[1]}");
 Console.WriteLine($"First: {fraudulentOrderIDs[2]}");

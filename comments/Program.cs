ba6d0fb10dd1c995bb344947134ecb047f670593
DC-21 <cholahkuboko@gmail.com>
2023-10-26 09:27:50 +0200

    arrays

---
+++ comments/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+Random random = new Random();
+string[] orderIds = new string[5];
+//loop through each blank order
+for(int i=0; i<orderIds.Length; i++)
+{
+      // Get a random value that equates to ASCII letters A through E
+      int prefixValue = random.Next(65,70);
+      //convert random value into char then string
+      string prefix = Convert.ToChar(prefixValue).ToString();
+       // Create a random number, pad with zeroes
+       string suffix = random.Next(1,3000).ToString("000");
+       //combine prefix and suffix
+       orderIds[i] = prefix + suffix;
+}
+// Print out each orderID
+foreach (var orderID in orderIds)
+{
+    Console.WriteLine(orderID);
+}

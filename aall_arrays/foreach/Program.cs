3fb162efb9e5e31a91c934f707164bd364665eb0
DC-21 <cholahkuboko@gmail.com>
2023-10-26 08:22:55 +0200

    arrays

--- aall_arrays/foreach/Program.cs
+++ aall_arrays/foreach/Program.cs
@@ -1,6 +1,19 @@
-﻿string[] myName = {"chola","chama","chansa"};
+﻿//strings
+string[] myName = {"chola","chama","chansa"};
 
 foreach(string name in myName)
 {
     Console.WriteLine(name);
-}
+};
+
+//integers
+int[] inventory = {12,13,14,15,16,17,18,19,20};
+int sum = 0;
+int bin = 0;
+foreach(int item in inventory)
+{
+    sum+=item;
+    bin++;
+    Console.WriteLine($"bin {bin} = {item} items running  (Running total: {sum})");
+};
+Console.WriteLine($"we have {sum} items in inventory");

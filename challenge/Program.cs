4d5a76f5e4b456b9b4f6d8476fff8ff9462c8a96
DC-21 <cholahkuboko@gmail.com>
2023-10-26 10:09:14 +0200

    arrays

---
+++ challenge/Program.cs
@@ -0,0 +1,10 @@
+﻿string me = "me nah lie";
+char[] newMessage = me.ToCharArray();
+Array.Reverse(newMessage);
+int dr = 0;
+
+foreach(char letter in newMessage){if(letter=='i') {dr++;}}
+
+string new_message = new String(newMessage);
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {dr} times");

a9191369a02bc314b136c5fdd7d7f4df48aa156e
DC-21 <cholahkuboko@gmail.com>
2023-10-26 07:48:39 +0200

    arrays

---
+++ arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0]="001a";
+fraudulentOrderIDs[1]="002b";
+fraudulentOrderIDs[2]="003c";
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"First: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"First: {fraudulentOrderIDs[2]}");
+
+//reassign values
+fraudulentOrderIDs[0]="2100DiCE";
+Console.WriteLine($"Reassigned value: {fraudulentOrderIDs[0]}");
